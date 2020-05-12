66ed51046b67eccbaabc6c04ea620fe648970297
Chuck Lu <chuck.lu@qq.com>
2020-05-12 16:31:57 +0800

    start task by Task.Start and wait all of them by Task.WaitAll

--- ConcurrentTestApp/Program.cs
+++ ConcurrentTestApp/Program.cs
@@ -12,7 +12,6 @@ namespace ConcurrentTestApp
         private static readonly List<string> AddSuccessfully = new List<string>();
         private static readonly List<string> RemoveSuccessfully = new List<string>();
         private static readonly List<string> IterateString = new List<string>();
-        private static int _tryRemoveCount;
         private static readonly Random Random = new Random();
         private static readonly List<Task> TaskList = new List<Task>();
         static void Main(string[] args)
@@ -26,15 +25,15 @@ namespace ConcurrentTestApp
                     Task task;
                     if (j == 1)
                     {
-                        task = new Task(DoTask1, i);
+                        task = new Task(AddTask, i);
                     }
                     else if (j == 2)
                     {
-                        task = new Task(DoTask1, i);
+                        task = new Task(RemoveTask, i);
                     }
                     else if (j == 3)
                     {
-                        task = new Task(DoTask1, i);
+                        task = new Task(IterateTask, i);
                         j = 0;
                     }
                     else
@@ -43,6 +42,13 @@ namespace ConcurrentTestApp
                     }
                     TaskList.Add(task);
                 }
+
+                foreach (var task in TaskList)
+                {
+                    task.Start();
+                }
+
+                Task.WaitAll(TaskList.ToArray());
 
                 Console.WriteLine($"addSuccessfully.Count = {AddSuccessfully.Count}");
                 foreach (var item in AddSuccessfully)
@@ -72,46 +78,44 @@ namespace ConcurrentTestApp
             }
         }
 
-        public static void DoTask1(object obj)
+        public static void AddTask(object obj)
         {
-            Console.WriteLine($"DoTask1 with {obj} Thread id = {Thread.CurrentThread.ManagedThreadId}");
+            Console.WriteLine($"AddTask with {obj} Thread id = {Thread.CurrentThread.ManagedThreadId}");
             for (int i = 0; i < 100; i++)
             {
                 var flag = Dictionary.TryAdd(i, i);
                 if (flag)
                 {
-                    AddSuccessfully.Add($"DoTask1 with {obj} add {i} successfully {DateTime.Now:yyyy-MM-dd HH:mm:ss.fff}");
+                    AddSuccessfully.Add($"AddTask with {obj} add {i} successfully {DateTime.Now:yyyy-MM-dd HH:mm:ss.fff}");
                     //Console.WriteLine();
                 }
                 Thread.Sleep(1);
             }
         }
 
-        public static void DoTask2(object obj)
+        public static void RemoveTask(object obj)
         {
-            _tryRemoveCount++;
             Console.WriteLine(
-                $"DoTask2 with {obj} Thread id = {Thread.CurrentThread.ManagedThreadId}, count = {Dictionary.Count}");
+                $"RemoveTask with {obj} Thread id = {Thread.CurrentThread.ManagedThreadId}, count = {Dictionary.Count}");
             var toRemove = Random.Next(0, 100);
             for (int i = 0; i < 100; i++)
             {
                 var flag = Dictionary.TryRemove(toRemove, out _);
                 if (flag)
                 {
-                    RemoveSuccessfully.Add($"DoTask2 with {obj} remove {toRemove} successfully. {DateTime.Now:yyyy-MM-dd HH:mm:ss.fff}");
+                    RemoveSuccessfully.Add($"RemoveTask with {obj} remove {toRemove} successfully. {DateTime.Now:yyyy-MM-dd HH:mm:ss.fff}");
                 }
                 Thread.Sleep(1);
             }
         }
 
-        public static void DoTask3(object obj)
+        public static void IterateTask(object obj)
         {
-            _tryRemoveCount++;
             Console.WriteLine(
-                $"DoTask3 with {obj} Thread id = {Thread.CurrentThread.ManagedThreadId}, count = {Dictionary.Count}");
+                $"IterateTask with {obj} Thread id = {Thread.CurrentThread.ManagedThreadId}, count = {Dictionary.Count}");
             foreach (var item in Dictionary)
             {
-                IterateString.Add($"DoTask3 with {obj} iterate {item.Key} {DateTime.Now:yyyy-MM-dd HH:mm:ss.fff}");
+                IterateString.Add($"IterateTask with {obj} iterate {item.Key} {DateTime.Now:yyyy-MM-dd HH:mm:ss.fff}");
                 Thread.Sleep(2);
             }
         }
